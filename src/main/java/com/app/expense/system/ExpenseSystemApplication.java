4d6190aae49b2a1c39116781776c57fc57ea6654
avdhutk4 <avdhutk4@gmail.com>
2024-06-28 17:50:04 +0530

    unknow_property_sources_changed

--- src/main/java/com/app/expense/system/ExpenseSystemApplication.java
+++ src/main/java/com/app/expense/system/ExpenseSystemApplication.java
@@ -1,13 +1,25 @@
 package com.app.expense.system;
 
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class ExpenseSystemApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ExpenseSystemApplication.class, args);
+	}
+
+	@Bean
+	public ObjectMapper getObjectMapper() {
+		ObjectMapper objectMapper = new ObjectMapper();
+		objectMapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES,false);
+		objectMapper.configure(SerializationFeature.INDENT_OUTPUT,true);
+		return objectMapper;
 	}
 
 }
--- src/main/java/com/app/expense/system/controller/ExpenseController.java
+++ src/main/java/com/app/expense/system/controller/ExpenseController.java
@@ -3,10 +3,14 @@ package com.app.expense.system.controller;
 import com.app.expense.system.customExceptions.ExpenseNotFoundException;
 import com.app.expense.system.entity.Expense;
 import com.app.expense.system.service.ExpenseService;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.extern.slf4j.Slf4j;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -19,16 +23,28 @@ public class ExpenseController {
     @Autowired
     private ExpenseService expenseService;
 
+    @Autowired
+    private ObjectMapper objectMapper;
+
     private static final Logger logger =
             LoggerFactory.getLogger(
                     ExpenseController.class);
 
 
-    @PostMapping("/")
+
+    @PostMapping("/save/")
     public Expense saveExpense(@RequestBody Expense expense){
         return expenseService.saveExpense(expense);
     }
 
+
+    @PostMapping("/")
+    public ResponseEntity<?> saveUnknownExpense(@RequestBody String unknownExpense) throws JsonProcessingException {
+        Expense expense = objectMapper.readValue(unknownExpense, Expense.class);
+
+        expenseService.saveExpense(expense);
+        return ResponseEntity.status(HttpStatus.OK).body(expense);
+    }
 
     @GetMapping("/{id}")
     public Expense getExpenseById(@PathVariable("id") Long expenseId) throws ExpenseNotFoundException {
